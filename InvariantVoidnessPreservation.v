7d29d1fb84910b60d199b52da1167c3371665d02
Erik Ernst <eernst@google.com>
2017-03-23 09:00:25 +0100

    Inlined voidness/annotationVoidness into the invariant rules

--- InvariantVoidnessPreservation.v
+++ InvariantVoidnessPreservation.v
@@ -12,25 +12,22 @@ Require Export Voidness.
 Module VoidnessPreservationBase (Import MyVoidness : VoidnessSig).
 
   Inductive VoidnessPreserves : DartType -> DartType -> Prop :=
-  | vp_0_any : ∀ dt1 dt2,
-    voidness dt1 = vt_0 ->
-    VoidnessPreserves dt1 dt2
+  | vp_0_any : ∀ dt,
+    VoidnessPreserves dt dt_dynamic
   | vp_any_1 : ∀ dt1 dt2,
     annotationVoidness dt2 = vt_1 ->
     VoidnessPreserves dt1 dt2
   | vp_class : ∀ dtypes1 dtypes2,
     VoidnessClassTypesPreserve dtypes1 dtypes2 ->
     VoidnessPreserves (dt_class dtypes1) (dt_class dtypes2)
-  | vp_class_0 : ∀ dtypes dt,
-    annotationVoidness dt = vt_0 ->
-    VoidnessPreserves (dt_class dtypes) dt
+  | vp_class_0 : ∀ dtypes,
+    VoidnessPreserves (dt_class dtypes) dt_dynamic
   | vp_function : ∀ ret1 ret2 args1 args2,
     VoidnessPreserves ret1 ret2 ->
     VoidnessPreservesPairwise args2 args1 ->
     VoidnessPreserves (dt_function ret1 args1) (dt_function ret2 args2)
-  | vp_function_0 : ∀ ret args dt,
-    annotationVoidness dt = vt_0 ->
-    VoidnessPreserves (dt_function ret args) dt
+  | vp_function_0 : ∀ ret args,
+    VoidnessPreserves (dt_function ret args) dt_dynamic
 
   with VoidnessPreservesPairwise : list DartType -> list DartType -> Prop :=
   | vpp_nil : VoidnessPreservesPairwise nil nil
@@ -82,3 +79,91 @@ Module VoidnessPreservationBase (Import MyVoidness : VoidnessSig).
   Hint Unfold TypeVoidnessPreserves.
 
 End VoidnessPreservationBase.
+
+(* In VoidnessPreserves above, we have replaced a couple of requirements by
+ * equivalent ones; the sections below check that they are indeed equivalent *)
+
+Module StrictVoidnessPreservation := VoidnessPreservationBase Voidness.StrictVoidness.
+Module NormalVoidnessPreservation := VoidnessPreservationBase Voidness.NormalVoidness.
+
+Module Type CheckSig (MyVoidness : VoidnessSig).
+
+  Parameter voidness0IsDynamic :
+    forall dt, MyVoidness.voidness dt = vt_0 -> dt = dt_dynamic.
+  Parameter annotationVoidness0IsDynamic :
+    forall dt, MyVoidness.annotationVoidness dt = vt_0 -> dt = dt_dynamic.
+  Parameter annotationVoidness1IsVoidOrVar :
+    forall dt, MyVoidness.annotationVoidness dt = vt_1 ->
+               (dt = dt_dynamic \/ dt = dt_void \/ exists n, dt = dt_variable n).
+
+End CheckSig.
+
+Module CheckStrict <: CheckSig StrictVoidness.
+
+  Import StrictVoidness.
+
+  Lemma voidness0IsDynamic :
+    forall dt, voidness dt = vt_0 -> dt = dt_dynamic.
+  Proof.
+    intro dt; destruct dt; intro H; auto; inversion H.
+  Qed.
+
+  Lemma annotationVoidness0IsDynamic :
+    forall dt, annotationVoidness dt = vt_0 -> dt = dt_dynamic.
+  Proof.
+    intro dt; destruct dt; intro H; auto; inversion H.
+  Qed.
+
+  Lemma annotationVoidness1IsVoidOrVarAux :
+    forall dt, annotationVoidness dt = vt_1 ->
+               (dt = dt_void \/ exists n, dt = dt_variable n).
+  Proof.
+    intro dt; destruct dt; intro H; auto;
+      inversion H; try inversion H0; try inversion H1.
+    right. exists n. reflexivity.
+  Qed.
+
+  Lemma annotationVoidness1IsVoidOrVar :
+    forall dt, annotationVoidness dt = vt_1 ->
+               (dt = dt_dynamic \/ dt = dt_void \/ exists n, dt = dt_variable n).
+  Proof.
+    intro dt; destruct dt; intro H; auto;
+      inversion H; try inversion H0; try inversion H1.
+    right. apply annotationVoidness1IsVoidOrVarAux. assumption.
+  Qed.
+
+  Eval cbv in annotationVoidness dt_dynamic. (* vt_0 *)
+  Eval cbv in annotationVoidness dt_void. (* vt_1 *)
+  Eval cbv in annotationVoidness (dt_variable A). (* vt_1 *)
+
+End CheckStrict.
+
+Module CheckNormal : CheckSig NormalVoidness.
+
+  Import NormalVoidness.
+
+  Lemma voidness0IsDynamic :
+    forall dt, voidness dt = vt_0 -> dt = dt_dynamic.
+  Proof.
+    intro dt; destruct dt; intro H; auto; inversion H.
+  Qed.
+
+  Lemma annotationVoidness0IsDynamic :
+    forall dt, annotationVoidness dt = vt_0 -> dt = dt_dynamic.
+  Proof.
+    intro dt; destruct dt; intro H; auto; inversion H.
+  Qed.
+
+  Lemma annotationVoidness1IsVoidOrVar :
+    forall dt, annotationVoidness dt = vt_1 ->
+               (dt = dt_dynamic \/ dt = dt_void \/ exists n, dt = dt_variable n).
+  Proof.
+    intro dt; destruct dt; intro H; auto; inversion H.
+    - right. right. exists n. reflexivity.
+  Qed.
+
+  Eval cbv in annotationVoidness dt_dynamic. (* vt_1 *)
+  Eval cbv in annotationVoidness dt_void. (* vt_1 *)
+  Eval cbv in annotationVoidness (dt_variable A). (* vt_1 *)
+
+End CheckNormal.
