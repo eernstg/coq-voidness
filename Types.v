44c5fde7222e33972dc370114f5a4df95350cc16
Erik Ernst <eernst@google.com>
2017-04-11 16:36:47 +0200

    Changed DartType and related types to use a single Inductive group such that we can get a proper induction principle; created that induction principle

--- Types.v
+++ Types.v
@@ -4,17 +4,32 @@
 
 Require Import Utf8.
 Require Import Coq.Program.Basics.
-Require Import List.
 
 Inductive Name : Set := Object | A | B | N | C1 | T | Iterable | List.
+
+Unset Elimination Schemes.
 
 Inductive DartType : Set :=
 | dt_void : DartType
 | dt_dynamic : DartType
-| dt_class : list (Name * list DartType) -> DartType (* Incl. all clausal supertypes *)
-| dt_function : DartType -> list DartType -> DartType
+| dt_class : NameDartTypes -> DartType (* Incl. all clausal supertypes *)
+| dt_function : DartType -> DartTypes -> DartType
 | dt_variable : Name -> DartType (* Type parameter with no bound: can be void *)
-| dt_bottom : DartType.
+| dt_bottom : DartType
 
-Definition ClassType : Type := (Name * (list DartType))%type.
-Definition ClassTypes : Type := list ClassType.
+with DartTypes : Set :=
+| dts_nil : DartTypes
+| dts_cons : DartType -> DartTypes -> DartTypes
+
+with NameDartTypes : Set :=
+| ndts_nil : NameDartTypes
+| ndts_cons : (Name * DartTypes) -> NameDartTypes -> NameDartTypes.
+
+Scheme DartType_ind := Induction for DartType Sort Prop
+  with DartTypes_ind := Induction for DartTypes Sort Prop
+  with NameDartTypes_ind := Induction for NameDartTypes Sort Prop.
+
+Combined Scheme DartType_mutind from
+  DartType_ind, DartTypes_ind, NameDartTypes_ind.
+
+Set Elimination Schemes.
